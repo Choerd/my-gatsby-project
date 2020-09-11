e85baebba7771483001748d26eddceacbcfaa7f8
Choerd <45365598+Choerd@users.noreply.github.com>
2020-09-11 15:35:05 +0200

    setup basic components

---
+++ src/components/layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Header from './header';
+import Footer from './footer';
+
+export default function Layout({ children }) {
+    return (
+        <>
+            <Header />
+            {children}
+            <Footer />
+        </>
+    );
+}
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
