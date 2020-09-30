99d037a1d1fb25331fd8e3d612ac1353b920f3bc
Choerd <45365598+Choerd@users.noreply.github.com>
2020-09-30 23:59:22 +0200

    added 404

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from 'components/layout';
+
+const notFound = () => {
+  return (
+    <Layout>
+      <div>404</div>
+    </Layout>
+  );
+};
+
+export default notFound;
